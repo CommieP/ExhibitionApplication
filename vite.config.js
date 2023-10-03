e4925f074a7a556a59fa61c3f90d4b28f7b66185
AdityaDeCW <67094373+CommieP@users.noreply.github.com>
2023-10-03 18:16:30 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import gltf from 'vite-plugin-gltf';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [react()],
+  plugins: [react(), gltf()],
 })
