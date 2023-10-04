bae0e04209c5b566db74d841443cc267275720c9
AdityaDeCW <67094373+CommieP@users.noreply.github.com>
2023-10-04 17:19:57 +0530

    Update popUp.jsx

--- src/HomePage/popUp.jsx
+++ src/HomePage/popUp.jsx
@@ -1,72 +1,21 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const PopUp = ({handleClose}) => {
     const index = useSelector((state) => state.IndexReducer.value);
+
+    let navigate = useNavigate();
 
     const handleClick = () => {
         handleClose();
     }
 
     const handleDesktop = () => {
-        if(index == 0){
-            location.assign("https://commiep.github.io/PCLinks/");
-        } else if (index == 1){
-            location.assign("https://commiep.github.io/PCLinks/desk1.html");
-        } else if (index == 2){
-            location.assign("https://commiep.github.io/PCLinks/desk2.html");
-        } else if (index == 3){
-            location.assign("https://commiep.github.io/PCLinks/desk3.html");
-        } else if (index == 4){
-            location.assign("https://commiep.github.io/PCLinks/desk4.html");
-        } else if (index == 5){
-            location.assign("https://commiep.github.io/PCLinks/desk5.html");
-        } else if (index == 8){
-            location.assign("https://commiep.github.io/PCLinks/desk8.html");
-        } else if (index == 9){
-            location.assign("https://commiep.github.io/PCLinks/desk9.html");
-        } else if (index == 10){
-            location.assign("https://commiep.github.io/PCLinks/desk10.html");
-        } else if (index == 11){
-            location.assign("https://commiep.github.io/PCLinks/desk11.html");
-        } else if (index == 12){
-            location.assign("https://commiep.github.io/PCLinks/desk12.html");
-        } else if (index == 14){
-            location.assign("https://commiep.github.io/PCLinks/desk14.html");
-        } else if (index == 15){
-            location.assign("https://commiep.github.io/PCLinks/desk15.html");
-        } else if (index == 16){
-            location.assign("https://commiep.github.io/PCLinks/desk16.html");
-        }
+        navigate("/desktop")
     }
 
     const handleVR = () => {
-        if(index == 0){
-            location.assign("https://commiep.github.io/PCLinks/vr0.html");
-        } else if (index == 1){
-            location.assign("https://commiep.github.io/PCLinks/vr1.html");
-        } else if (index == 2){
-            location.assign("https://commiep.github.io/PCLinks/vr2.html");
-        } else if (index == 3){
-            location.assign("https://commiep.github.io/PCLinks/vr3.html");
-        } else if (index == 4){
-            location.assign("https://commiep.github.io/PCLinks/vr4.html");
-        } else if (index == 5){
-            location.assign("https://commiep.github.io/PCLinks/vr5.html");
-        } else if (index == 8){
-            location.assign("https://commiep.github.io/PCLinks/vr8.html");
-        } else if (index == 9){
-            location.assign("https://commiep.github.io/PCLinks/vr9.html");
-        } else if (index == 11){
-            location.assign("https://commiep.github.io/PCLinks/vr11.html");
-        } else if (index == 12){
-            location.assign("https://commiep.github.io/PCLinks/vr12.html");
-        } else if (index == 14){
-            location.assign("https://commiep.github.io/PCLinks/vr14.html");
-        } else if (index == 15){
-            location.assign("https://commiep.github.io/PCLinks/vr15.html");
-        } else if (index == 16){
-            location.assign("https://commiep.github.io/PCLinks/vr16.html");
-        }
+        navigate("/VRCanvas")
     }
 
     return (
